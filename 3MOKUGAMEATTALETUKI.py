0fec9b08a9596b1b3bf4ce9bebe53f437a2ac690
mirai-work <mfkensub@gmail.com>
2026-06-26 09:54:40 +0900

    Update 3MOKUGAMEATTALETUKI.py

--- 3MOKUGAMEATTALETUKI.py
+++ 3MOKUGAMEATTALETUKI.py
@@ -215,7 +215,7 @@ class Othello25:
             case (True, False): self.status = 1
             case (False, True): self.status = 2
             case _: self.status = 3
-        self.scene = "RESULT_START"; self.transition_timer = 90
+        self.scene = "RESULT_START"; self.transition_timer = 300
         if js:
             try:
                 match self.status:
